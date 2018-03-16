33cac24507fd411c88082b382d516d1e92719011
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-16 10:02:04 -0700

    initial setup

---
+++ spec/super_galactic_age_calculator-spec.js
@@ -0,0 +1,9 @@
+// import { Constructor } from './../src/super_galactic_age_calculator.js'
+
+describe('', function() {
+
+  it('', function() {
+    // var example = new Constructor(parameters)
+    // expect(example.method()).toEqual('');
+  });
+});
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { **insert prototype name** } from './super_galactic_age_calculator.js';
+
+$(document).ready(function() {
+
+});
