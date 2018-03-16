410ef3dada2ad7f84e661e88b9d7d2d1c5f13c43
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-03-16 11:43:06 -0700

    take two dates and determine the difference in seconds between the two

--- spec/super_galactic_age_calculator-spec.js
+++ spec/super_galactic_age_calculator-spec.js
@@ -12,7 +12,7 @@ describe('', function() {
   });
 
   it('Takes two dates and determines the difference, in seconds, between the two. (This way a user can enter a birthdate and the application can compare the birthdate against the current time to come up with an accurate age.)', function() {
-    expect(char.secondsBetweenDates("4/19/1986", "03/15/2018")).toEqual(1006815600);
+    expect(char.secondsBetweenDates("2/19/1993", "03/15/2018")).toEqual(790988400);
   });
 
 });
